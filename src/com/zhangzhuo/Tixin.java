4b792955e20b5cc700788abf08bca4b46a769dea
zhangzhuo <1347088550@qq.com>
2020-11-22 20:39:53 +0800

    提交

---
+++ src/com/zhangzhuo/Tixin.java
@@ -0,0 +1,19 @@
+package com.zhangzhuo;
+
+import java.util.Scanner;
+
+public class Tixin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int A;
+		int B;
+		int H;
+		Scanner in = new Scanner(System.in);
+		A = in.nextInt();
+		B = in.nextInt();
+		H = in.nextInt();
+		System.out.println((A + B) * H / 2);
+	}
+
+}
